8feb9391b7fcf63958c8da7b9ce5dec6c27e0625
toulbox <tolojanaharypatrickandre@gmail.com>
2026-03-08 15:05:34 +0400

    feat(api): implement PrismaService with pg driver adapter

--- apps/api/src/shared/database/prisma.service.ts
+++ apps/api/src/shared/database/prisma.service.ts
@@ -1,13 +1,22 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
-export class PrismaService implements OnModuleInit, OnModuleDestroy
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
 {
-  async onModuleInit() {
 
+  constructor() {
+   const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 
   async onModuleDestroy() {
-
+    await this.$disconnect();
   }
 }
